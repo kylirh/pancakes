0a5c29b5a214e86ca307094e9d057c16893d6647
Kylir Horton <kylirh@gmail.com>
2018-12-27 19:52:42 -0700

    Implement makeHappy algorithm completely

--- PancakeStack.js
+++ PancakeStack.js
@@ -16,8 +16,8 @@ class PancakeStack {
         topStack.reverse();
 
         for (let i in topStack) {
-            let isHappyCake = (topStack[i] == HAPPYCAKE);
-            topStack[i] = isHappyCake ? SADCAKE : HAPPYCAKE;
+            let isHappycake = (topStack[i] == HAPPYCAKE);
+            topStack[i] = isHappycake ? SADCAKE : HAPPYCAKE;
         }
 
         this._pancakes.splice(0, topStack.length, ...topStack);
@@ -25,7 +25,25 @@ class PancakeStack {
     }
 
     makeHappy() {
-        // TODO: Implement flipping pancakes to all happy algorithm here.
+        // Find the last sadcake or return because the stack is all happycakes!
+        let lastSadcakeIndex = this._pancakes.lastIndexOf(SADCAKE);
+        if (lastSadcakeIndex == -1) {
+            return;
+        }
+
+        // If the top is a happycake, find the longest streak of happycakes from the top and flip them all.
+        if (this._pancakes[0] == HAPPYCAKE) {
+            let firstSadIndex = this._pancakes.indexOf(SADCAKE);
+            this.flipAt(firstSadIndex - 1);
+        }
+
+        // If the top is a sadcake, flip all the pancakes down to and including the last sadcake.
+        else {
+            this.flipAt(lastSadcakeIndex);
+        }
+
+        // ...and repeat!
+        this.makeHappy();
     }
 }
 
