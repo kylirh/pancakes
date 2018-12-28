7aeb612b448da553f1f01126ec72e4fa6c1bb343
Kylir Horton <kylirh@gmail.com>
2018-12-27 19:07:22 -0700

    Add ability to flip a stack of pancakes

--- PancakeStack.js
+++ PancakeStack.js
@@ -1,10 +1,21 @@
+const HAPPYCAKE = '+';
+const SADCAKE = '-';
+
 class PancakeStack {
     constructor(pancakes) {
-        this._pancakes = pancakes;
+        this._pancakes = Array.from(pancakes);
     }
 
     toString() {
-        return this._pancakes;
+        return this._pancakes.join('');
+    }
+
+    flip() {
+        this._pancakes.reverse();
+        for (let index in this._pancakes) {
+            let isHappyCake = (this._pancakes[index] == HAPPYCAKE);
+            this._pancakes[index] = isHappyCake ? SADCAKE : HAPPYCAKE;
+        }
     }
 }
 
--- test/test.js
+++ test/test.js
@@ -2,9 +2,19 @@ const assert = require('assert');
 const PancakeStack = require('../PancakeStack');
 
 describe('PancakeStack', function() {
+    const initialState = '+--+-+';
+    let pancakeStack;
+
+    beforeEach(function() {
+        pancakeStack = new PancakeStack(initialState);
+    });
+
     it('can return its state as a string', function() {
-        let initialState = '+--+-+';
-        let pancakeStack = new PancakeStack(initialState);
-        assert.equal(initialState, pancakeStack.toString());
+        assert.equal(initialState, pancakeStack);
+    });
+
+    it('can flip a stack of pancakes', function() {
+        pancakeStack.flip();
+        assert.equal(pancakeStack, '-+-++-');
     });
 });
